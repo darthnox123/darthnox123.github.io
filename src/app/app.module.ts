5fc709c4a8adb7d1c3a2ee7a4794d37053dc3691
FRancisco <francisco.trindade@knowledgeworks.pt>
2022-05-23 16:15:35 +0100

    [bugfixes]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
-      HeaderComponent
+      HeaderComponent,
+      SkillsComponent
    ],
   imports: [
     BrowserModule
