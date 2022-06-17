7f191dde49b3ae5b02ba5744c5f8f8e688b652d3
FRancisco <francisco.trindade@knowledgeworks.pt>
2022-06-17 18:38:44 +0100

    enviroment test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   token: '0add6d5d09msh50759a72dae9376p10398djsn3f6f54881f75',
   email: 'francisco__trindade@hotmail.com',
-  github_token: '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'
+  github_token: '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'
 };
