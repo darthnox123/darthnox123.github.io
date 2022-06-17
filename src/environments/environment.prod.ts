cc70f97c838dd8c0283b64f6a7fe04c66801c51c
FRancisco <francisco.trindade@knowledgeworks.pt>
2022-06-17 18:41:57 +0100

    [enviroment-test]

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   token: '0add6d5d09msh50759a72dae9376p10398djsn3f6f54881f75',
   email: 'francisco__trindade@hotmail.com',
-  github_token: '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'
+  github_token: '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'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   production: false,
   token: '0add6d5d09msh50759a72dae9376p10398djsn3f6f54881f75',
   email: 'francisco__trindade@hotmail.com',
-  github_token: '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'
+  github_token: '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'
 };
 
 /*
