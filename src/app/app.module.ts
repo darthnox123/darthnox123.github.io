7c488afe3dad48280dd5dc0f53f922933a2dd278
darthnox123 <francisco__trindade@hotmail.com>
2026-06-06 21:06:29 +0100

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { HeaderComponent } from './header/header.component';
 import { SkillsComponent } from './skills/skills.component';
 import { TimelineComponent } from './timeline/timeline.component';
 import { IdeiasComponent } from './ideias/ideias.component';
+import { FeaturedComponent } from './featured/featured';
 import { ProjectsService } from '@services';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SkillsComponent, TimelineComponent, IdeiasComponent],
+  declarations: [AppComponent, HeaderComponent, SkillsComponent, TimelineComponent, IdeiasComponent, FeaturedComponent],
   imports: [BrowserModule],
   providers: [ProjectsService, provideHttpClient()],
   bootstrap: [AppComponent],
---
+++ src/app/featured/featured.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured',
+  standalone: false,
+  templateUrl: './featured.html',
+  styleUrl: './featured.css',
+})
+export class FeaturedComponent {}
